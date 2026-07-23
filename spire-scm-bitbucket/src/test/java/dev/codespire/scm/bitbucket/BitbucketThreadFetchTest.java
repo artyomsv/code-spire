093a22aae144d20fcb9ef96d739ea593b4e3e9e9
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-07-23 20:29:06 +0200

    Cover Bitbucket thread pagination and bot-attribution fail-open

--- spire-scm-bitbucket/src/test/java/dev/codespire/scm/bitbucket/BitbucketThreadFetchTest.java
+++ spire-scm-bitbucket/src/test/java/dev/codespire/scm/bitbucket/BitbucketThreadFetchTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -70,5 +71,44 @@ class BitbucketThreadFetchTest {
         assertTrue(t.messages().get(0).fromBot());          // BOT-1
         assertFalse(t.messages().get(1).fromBot());
         assertEquals("why?", t.messages().get(1).text());
+    }
+
+    @Test
+    void followsPaginationViaNextAndCollectsFullSubtree() {
+        server.stubFor(get(urlEqualTo(COMMENTS + "?pagelen=100&page=1")).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json").withBody("""
+                        { "values": [
+                          { "id": 100, "content": { "raw": "possible NPE" },
+                            "user": { "account_id": "BOT-1" }, "inline": { "path": "src/App.java", "to": 42 } },
+                          { "id": 200, "parent": { "id": 100 }, "content": { "raw": "reply on page1" },
+                            "user": { "account_id": "HUM-9" } } ],
+                          "next": "http://localhost/anything" }""")));
+        server.stubFor(get(urlEqualTo(COMMENTS + "?pagelen=100&page=2")).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json").withBody("""
+                        { "values": [
+                          { "id": 400, "parent": { "id": 200 }, "content": { "raw": "reply on page2" },
+                            "user": { "account_id": "HUM-9" } } ] }""")));
+
+        ThreadTranscript t = sink.fetchThread(REPO, 7, new ThreadRef("100"));
+
+        assertEquals("src/App.java", t.path());
+        assertEquals(42, t.line());
+        assertEquals(3, t.messages().size());               // 100 + 200 (page 1) + 400 (page 2)
+        assertEquals("reply on page2", t.messages().get(2).text());
+    }
+
+    @Test
+    void degradesToUnattributedWhenBotAccountLookupFails() {
+        server.stubFor(get(urlPathEqualTo("/user")).willReturn(aResponse().withStatus(503)));
+        server.stubFor(get(urlPathEqualTo(COMMENTS)).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json").withBody("""
+                        { "values": [
+                          { "id": 100, "content": { "raw": "possible NPE" },
+                            "user": { "account_id": "BOT-1" }, "inline": { "path": "src/App.java", "to": 42 } } ] }""")));
+
+        ThreadTranscript t = sink.fetchThread(REPO, 7, new ThreadRef("100"));
+
+        assertEquals(1, t.messages().size());
+        assertFalse(t.messages().get(0).fromBot());        // /user lookup failed -> botAccountId is "" -> no match
     }
 }
